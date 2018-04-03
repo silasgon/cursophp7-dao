b3aecfa6ac7e4314ef75aa5034ac592234e35aac
Silas Gon <silasidc@gmail.com>
2018-04-03 17:29:54 -0300

    Criação da classe Usuario

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 
-require_once ("config.php");
+require_once("config.php");
 
+/* Lista usuarios do BD
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
+
+$user = new Usuario();
+
+$user->loadbyId(1);
+
+echo $user;
 
