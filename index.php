76103557cba16957943c59006e8a5f05b8f39b39
Silas Gon <silasidc@gmail.com>
2018-04-04 16:53:02 -0300

    Atualizando a classe Usuario - Metodos de lista

--- index.php
+++ index.php
@@ -2,17 +2,34 @@
 
 require_once("config.php");
 
-/* Lista usuarios do BD
+/* Carrega um usuario
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
-*/
+
 
 $user = new Usuario();
 
 $user->loadbyId(1);
 
 echo $user;
+*/
 
+//Lista de usuarios
+
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+
+//Carrega uma lista de usuarios pelo login
+
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//Carrega um usuario usando logine a senha
+
+$usuario = new Usuario();
+$usuario->login("root","1234");
+echo $usuario;
